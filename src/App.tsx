da799b31cf75c0bbfe95e099985d5ee091505578
Anurag1096 <anuragqwerty09@outlook.com>
2024-04-01 19:52:12 +0530

    creating sidebar comp and necessary links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import Home from './pages/Home';
+import Analytics from './pages/Analytics';
+import Settings from './pages/Settings';
+import "./App.css"
 const App=()=>{
     return(
-
-        <div>
-           setting up react without any builder command ;asldkjf;lksajf
-        </div>
+        <Router>
+           <div className='app'>
+           <SideBar/>
+           <div className='main_content'>
+           <Routes>
+            <Route path="/"  element={<Home/>} />
+            <Route path="/analytics" element={<Analytics/>} />
+            <Route path="/settings" element={<Settings/>} />
+            {/* Add more routes as needed */}
+          </Routes>
+          </div>
+          </div>
+        </Router>
+       
     )
 }
 
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import "./sideBar.css"
+import { Link } from 'react-router-dom';
+
+const SideBar = () => {
+    const [showSidebar,setShowSidebar] = useState<boolean>(true)
+    const handleClick=()=>{
+      setShowSidebar(prevState=>!prevState)
+    }
+    return (<>
+        {/* <button className="sidebar_burger_button" onClick={handleClick}>burger</button> */}
+        {showSidebar && (
+                <section className="main_wrapper" aria-label="Sidebar">
+                    <ul className="list_container">
+                        <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/">Home</Link></li>
+                        <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/analytics">Analytics</Link></li>
+                        <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/settings">Settings</Link></li>
+                    </ul>
+                </section>
+            )}
+        </>
+    )
+}
+
+export default SideBar;
