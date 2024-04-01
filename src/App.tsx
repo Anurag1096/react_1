540d294efe499eb74a2fa9d7ac0f90fa3f81f963
Anurag1096 <anuragqwerty09@outlook.com>
2024-04-01 11:27:18 +0530

    creating react app setup without using create-react-app command or any other command

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App=()=>{
+    return(
+
+        <div>
+           setting up react without any builder command ;asldkjf;lksajf
+        </div>
+    )
+}
+
+export default App;
