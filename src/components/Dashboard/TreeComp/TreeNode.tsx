d32d1ecbf5da9d15d09dc7017a17081941135514
Anurag1096 <anuragqwerty09@outlook.com>
2024-04-26 22:14:24 +0530

    Feat: Recursive comp

--- src/components/Dashboard/TreeComp/TreeNode.tsx
+++ src/components/Dashboard/TreeComp/TreeNode.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TreeType } from './type'
 export const TreeNode = ({ node }: { node: TreeType }) => {
+  const [toggle, setToggle] = useState<boolean>(false)
+  const toggleExpanded = () => {
+    setToggle(prevState => !prevState)
+  }
   return (
     <div>
-      {node.name}
+      <div style={{cursor:"pointer"}} onClick={toggleExpanded}>
+        {node.children &&
+          <span>{toggle ? "⬆️" : '⬇️'}
+          </span>
+        }
+        {node.name}
+      </div>
+
       {/* Render child nodes if they exist */}
-      {node.children && node.children.map(child => (
-        <TreeNode key={child.id} node={child} />
-      ))}
+      {toggle && node.children && (
+        <div style={{ marginLeft: "20px" }}>
+          {node.children.map(child => (
+            <TreeNode key={child.id} node={child} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
