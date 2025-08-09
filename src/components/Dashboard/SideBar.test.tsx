e293288f8dbc6ff5518eaed36e1be67a3a416af8
Anurag1096 <anuragqwerty09@outlook.com>
2025-08-09 18:16:36 +0530

    wip: Adding test for link clicking

--- src/components/Dashboard/SideBar.test.tsx
+++ src/components/Dashboard/SideBar.test.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import {render,screen} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { vi } from 'vitest';
 import SideBar from './SideBar'
 
 
 
-test("Render a sideBar with with bar options",()=>{
-    const dClose=vi.fn()
-  
-     render(
-     <MemoryRouter>
-         <SideBar openSidebar={true} setCloseSidebar={dClose} />
-     </MemoryRouter>
+test("Render a sideBar with with bar options", () => {
+    const dClose = vi.fn()
+
+    render(
+        <MemoryRouter >
+            <SideBar openSidebar={true} setCloseSidebar={dClose} />
+        </MemoryRouter>
     );
- 
-     expect(screen.getByText(/home/i)).toBeInTheDocument();
-     expect(screen.getByText(/analytics/i)).toBeInTheDocument();
-     expect(screen.getByText(/settings/i)).toBeInTheDocument();
-     expect(screen.getByText(/chat/i)).toBeInTheDocument();
-     expect(screen.getByText(/dataList/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/home/i)).toBeInTheDocument();
+    expect(screen.getByText(/analytics/i)).toBeInTheDocument();
+    expect(screen.getByText(/settings/i)).toBeInTheDocument();
+    expect(screen.getByText(/chat/i)).toBeInTheDocument();
+    expect(screen.getByText(/dataList/i)).toBeInTheDocument();
 
 })
 
+test("Check for click on the sidebar options", () => {
+    //Arrange
+    const dClose = vi.fn()
+    render(
+        <MemoryRouter initialEntries={['/']}>
+            
+            <SideBar openSidebar={true} setCloseSidebar={dClose} />
+        </MemoryRouter>
+    );
 
+    // now to get the link tag for home
+    const homeLink = screen.getByText(/home/i);
+    const analyticsLink=screen.getByText(/analytics/i);
+    //Act
+    fireEvent.click(homeLink)
+    expect(homeLink.getAttribute('href')).toBe('/');
+    fireEvent.click(analyticsLink)
+    expect(analyticsLink.getAttribute('href')).toBe('/analytics');
+    //Assert
+
+
+})
--- src/components/Dashboard/SideBar.tsx
+++ src/components/Dashboard/SideBar.tsx
@@ -15,7 +15,7 @@ const SideBar = ({openSidebar,setCloseSidebar}:Props) => {
         <div className={openSidebar?`main_wrapper2`:"main_wrapper"} aria-label="Sidebar">
            {openSidebar && <button className="btn_close" onClick={handleClick}>close</button>}
                     <ul className="list_container">
-                        <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/">Home</Link></li>
+                        <li className="list_item"><Link  style={{ textDecoration: 'none' }} to="/">Home</Link></li>
                         <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/analytics">Analytics</Link></li>
                         <li className="list_item"><Link style={{ textDecoration: 'none' }} to="/settings">Settings</Link></li>
                         <li className="list_item"><Link style={{textDecoration:"none"}} to ='/chat'>Chat</Link></li>
